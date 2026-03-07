c993d93d660807216e4e5904ab69c2f66f3cff90
duvanleandro <dpedrazagonzalez@gmail.com>
2026-03-07 01:55:00 -0500

    se modifica port en webserver

--- webserver.py
+++ webserver.py
@@ -8,7 +8,7 @@ def index():
     return "Hello, World!"
 
 def run():  
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     server = Thread(target=run)
